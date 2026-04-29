0e2219ef9ebd44bfe6ec2f6191fc0e4977194ed2
Anton Burtasenkov <a.burtasenkov.work@gmail.com>
2026-04-29 14:42:38 +0200

    Added constants for starting board positions

---
+++ src/main/Constants.hpp
@@ -0,0 +1,26 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <stdint.h>
+
+namespace Engine {
+  namespace StartPos {
+    constexpr uint64_t WhitePawns   = 0x000000000000FF00ULL;
+    constexpr uint64_t WhiteKnights = 0x0000000000000042ULL;
+    constexpr uint64_t WhiteBishops = 0x0000000000000024ULL;
+    constexpr uint64_t WhiteRooks   = 0x0000000000000081ULL;
+    constexpr uint64_t WhiteQueen   = 0x0000000000000008ULL;
+    constexpr uint64_t WhiteKing    = 0x0000000000000010ULL;
+    constexpr uint64_t WhiteAll     = 0x000000000000FFFFULL;
+
+    constexpr uint64_t BlackPawns   = 0x00FF000000000000ULL;
+    constexpr uint64_t BlackKnights = 0x4200000000000000ULL;
+    constexpr uint64_t BlackBishops = 0x2400000000000000ULL;
+    constexpr uint64_t BlackRooks   = 0x8100000000000000ULL;
+    constexpr uint64_t BlackQueen   = 0x0800000000000000ULL;
+    constexpr uint64_t BlackKing    = 0x1000000000000000ULL;
+    constexpr uint64_t BlackAll     = 0xFFFF000000000000ULL;
+  }
+}
+
+#endif
