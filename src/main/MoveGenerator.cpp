1f7ac5251d3346e16f2675d429229f6c3dba005f
Anton Burtasenkov <a.burtasenkov.work@gmail.com>
2026-05-11 05:43:51 +0200

    Added placeholder methods. This allows to write unit tests for MoveGenerator class

--- src/main/MoveGenerator.cpp
+++ src/main/MoveGenerator.cpp
@@ -1,5 +1,42 @@
 #include "MoveGenerator.hpp"
 
+#include "MoveList.hpp"
+
 namespace Engine {
+
+  MoveList MoveGenerator::generate_legal_moves(const Board& board) {
+    (void)board;
+    return MoveList();
+  }
   
+  void MoveGenerator::generate_pawn_moves(const Board& board, MoveList& moves) {
+    (void)board;
+    (void)moves;
+  }
+
+  void MoveGenerator::generate_knight_moves(const Board& board, MoveList& moves) {
+    (void)board;
+    (void)moves;
+  }
+
+  void MoveGenerator::generate_bishop_moves(const Board& board, MoveList& moves) {
+    (void)board;
+    (void)moves;
+  }
+
+  void MoveGenerator::generate_rook_moves(const Board& board, MoveList& moves) {
+    (void)board;
+    (void)moves;
+  }
+
+  void MoveGenerator::generate_queen_moves(const Board& board, MoveList& moves) {
+    (void)board;
+    (void)moves;
+  }
+
+  void MoveGenerator::generate_king_moves(const Board& board, MoveList& moves) {
+    (void)board;
+    (void)moves;
+  }
+
 } // namespace Engine
